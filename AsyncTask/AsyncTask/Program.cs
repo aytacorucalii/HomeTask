e6c6f2aa37e824fdab6628e0cf50fbae2377662d
Aytacorucali <aytacho-ap215@code.edu.az>
2024-11-04 21:40:22 +0400

    Non-async and async Task

---
+++ AsyncTask/AsyncTask/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Net.Http;
+
+namespace AsyncTask
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Stopwatch sw = Stopwatch.StartNew();
+            List<string> urls = new List<string>()
+            {
+                "https://kontakt.az/telefoniya",
+                "https://www.soliton.az/search.php?q=telefon",
+                "https://irshad.az/mehsullar?q=telefon",
+                "https://irshad.az/mehsullar?q=telefon"
+
+            };
+             RetrieveDataNonAsync(urls);
+            sw.Stop();
+            Console.WriteLine(sw.ElapsedMilliseconds);
+            sw.Restart();
+            Console.WriteLine("--------------------------------------------------------------");
+            RetrieveDataAsync(urls).Wait();
+            Console.WriteLine(sw.ElapsedMilliseconds);
+        }
+        public static void RetrieveDataNonAsync(List<string> urls)
+        {
+            HttpClient client = new HttpClient();
+            foreach (var url in urls)
+            {
+
+                Console.WriteLine(client.GetStringAsync(url).Result.ToString());
+            }
+
+        }
+        public static async Task RetrieveDataAsync(List<string> urls)
+        {
+            HttpClient client = new HttpClient();
+            List<Task<string>> links = new List<Task<string>>();
+            foreach (var url in urls)
+            {
+                links.Add(client.GetStringAsync(url));
+                await Task.WhenAll(links);
+            }
+
+        }
+    }
+}
