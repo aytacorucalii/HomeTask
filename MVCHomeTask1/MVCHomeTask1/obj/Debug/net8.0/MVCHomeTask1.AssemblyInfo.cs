103bb564d134933de6dcee15363ce4e181534ce1
Aytacorucali <aytacho-ap215@code.edu.az>
2024-11-21 10:13:50 +0400

    Təmizlənmiş müvəqqəti fayllar və .gitignore əlavə edildi

--- MVCHomeTask1/MVCHomeTask1/obj/Debug/net8.0/MVCHomeTask1.AssemblyInfo.cs
+++ MVCHomeTask1/MVCHomeTask1/obj/Debug/net8.0/MVCHomeTask1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MVCHomeTask1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+159d0a4632a64f61303d6702b7d4723465c5c90a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a0637cdd5894235abe77f6c565e088cb2c3a0181")]
 [assembly: System.Reflection.AssemblyProductAttribute("MVCHomeTask1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MVCHomeTask1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- PurpleBuzzApp/PurpleBuzzApp/Models/user.cs
+++
@@ -1,5 +0,0 @@
-﻿namespace PurpleBuzzApp.Models;
-
-public class user
-{
-}
